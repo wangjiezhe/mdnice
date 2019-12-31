ebc5f91e9738b687268cf23eaa53386d0f6da347
wangjiezhe <wangjiezhe@gmail.com>
2019-12-31 12:10:27 +0800

    Initial commit

---
+++ manifest.json
@@ -0,0 +1,20 @@
+{
+  "short_name": "Markdown Nice",
+  "name": "Markdown Nice",
+  "icons": [
+    {
+      "src": "https://my-wechat.mdnice.com/mdnice/mdnice%20logo_20191007150129.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "https://my-wechat.mdnice.com/mdnice/mdnice%20logo_20191007150129.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "./index.html",
+  "display": "standalone",
+  "theme_color": "#000000",
+  "background_color": "#ffffff"
+}
---
+++ precache-manifest.7cff4690a3c85735344e741f25133b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b409de55bdd52dc870e561656f4c95",
+    "url": "/mdnice/index.html"
+  },
+  {
+    "revision": "1781143865eaf61316b0",
+    "url": "/mdnice/static/css/2.92824036.chunk.css"
+  },
+  {
+    "revision": "ff9f9885aa0960084a2e",
+    "url": "/mdnice/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1781143865eaf61316b0",
+    "url": "/mdnice/static/js/2.fdfcc9f4.chunk.js"
+  },
+  {
+    "revision": "ff9f9885aa0960084a2e",
+    "url": "/mdnice/static/js/main.074efff2.chunk.js"
+  },
+  {
+    "revision": "bfc67767783106a400de",
+    "url": "/mdnice/static/js/runtime-main.a4bac575.js"
+  }
+]);
